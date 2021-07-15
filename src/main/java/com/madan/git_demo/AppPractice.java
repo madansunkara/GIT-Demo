312e4ceed29da47301cd2cebf8805788d3cc9e08
madan.local <madan.sunkara@gmail.com>
2021-07-15 17:06:51 +0530

    initial commit

---
+++ src/main/java/com/madan/git_demo/AppPractice.java
@@ -0,0 +1,15 @@
+package com.madan.git_demo;
+
+public class AppPractice {
+	
+	public void addData(String message) {
+		System.out.println("---passed value is-------: "+ message);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("------Hello World------");
+
+	}
+
+}
